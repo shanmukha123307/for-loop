09a995119a7b8f07dd67a92bd6a03c1817f53a97
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-09 11:58:52 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,5 @@
+x=input("enter a string")
+y=('')
+for i in x:
+    y=i+y
+print(y)
---
+++ file3.py
@@ -0,0 +1,4 @@
+n=int(input("enter the number "))
+print("numbers from {0} to {1} are ".format(n,1))
+for x in range (n,0,-1):
+    print(x)
